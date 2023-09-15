ad7474a34301f42eb68730f1ee4ad15aeb187cc7
unknown <rafael.alves.ds@gmail.com>
2023-09-15 19:41:25 -0300

    Adicionado autocomplete nos campos de home-server e item-id

--- commands/utility/notify.js
+++ commands/utility/notify.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const notificationSchema = require('../../schemas/notification');
+const itemsIds = require('../../items');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,7 +9,8 @@ module.exports = {
 		.addStringOption(option =>
 			option.setName('item-id')
 				.setDescription('The ID of the item')
-				.setRequired(true))
+				.setRequired(true)
+                .setAutocomplete(true))
         .addStringOption(option =>
             option.setName('retainer')
                 .setDescription('The name of the retainer selling the item')
@@ -16,7 +18,31 @@ module.exports = {
         .addStringOption(option =>
             option.setName('home-server')
                 .setDescription('The name of your home server. Ex: Behemoth')
-                .setRequired(true)),
+                .setRequired(true)
+                .setAutocomplete(true)),
+    async autocomplete(interaction) {
+        const focusedOption = interaction.options.getFocused(true);
+        let choices;
+        let filtered;
+        if (focusedOption.name === 'item-id') {
+            choices = itemsIds;
+            // console.log(choices);
+            filtered = choices.filter(choice => choice.en.toLowerCase().includes(focusedOption.value.toLowerCase())).slice(0, 25);
+
+            await interaction.respond(
+                filtered.map(choice => ({ name: choice.en, value: choice.id.toString() })),
+            );
+
+        }
+        if (focusedOption.name === 'home-server') {
+            choices = ['Ravana', 'Bismarck', 'Asura', 'Belias', 'Pandaemonium', 'Shinryu', 'Unicorn', 'Yojimbo', 'Zeromus', 'Twintania', 'Brynhildr', 'Famfrit', 'Lich', 'Mateus', 'Omega', 'Jenova', 'Zalera', 'Zodiark', 'Alexander', 'Anima', 'Carbuncle', 'Fenrir', 'Hades', 'Ixion', 'Kujata', 'Typhon', 'Ultima', 'Valefor', 'Exodus', 'Faerie', 'Lamia', 'Phoenix', 'Siren', 'Garuda', 'Ifrit', 'Ramuh', 'Titan', 'Diabolos', 'Gilgamesh', 'Leviathan', 'Midgardsormr', 'Odin', 'Shiva', 'Atomos', 'Bahamut', 'Chocobo', 'Moogle', 'Tonberry', 'Adamantoise', 'Coeurl', 'Malboro', 'Tiamat', 'Ultros', 'Behemoth', 'Cactuar', 'Cerberus', 'Goblin', 'Mandragora', 'Louisoix', 'Spriggan', 'Sephirot', 'Sophia', 'Zurvan', 'Aegis', 'Balmung', 'Durandal', 'Excalibur', 'Gungnir', 'Hyperion', 'Masamune', 'Ragnarok', 'Ridill', 'Sargatanas', 'Sagittarius', 'Phantom', 'Alpha', 'Raiden', 'Marilith', 'Seraph', 'Halicarnassus', 'Maduin', '红玉海', '神意之地', '拉诺西亚', '幻影群岛', '萌芽池', '宇宙和音', '沃仙曦染', '晨曦王座', '白银乡', '白金幻象', '神拳痕', '潮风亭', '旅人栈桥', '拂晓之间', '龙巢神殿', '梦羽宝境', '紫水栈桥', '延夏', '静语庄园', '摩杜纳', '海猫茶屋', '柔风海湾', '琥珀原', '水晶塔', '银泪湖', '太阳海岸', '伊修加德', '红茶川', '黄金谷', '月牙湾', '雪松原', '카벙클', '초코보', '모그리', '톤베리', '펜리르'];
+            filtered = choices.filter(choice => choice.startsWith(focusedOption.value)).slice(0, 25);
+            await interaction.respond(
+                filtered.map(choice => ({ name: choice, value: choice })),
+            );
+        }
+
+    },
 	async execute(interaction) {
         const itemID = interaction.options.getString('item-id').trim();
         const homeServer = interaction.options.getString('home-server').trim();
